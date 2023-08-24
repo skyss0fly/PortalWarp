fe77b4ebc3c14a0f7d22d116fd14ff1dd864aa91
skyss0fly <100278495+skyss0fly@users.noreply.github.com>
2023-08-24 19:41:50 +1000

    Update Main.php
    register warp

--- src/skyss0fly/PortalWarp/Main.php
+++ src/skyss0fly/PortalWarp/Main.php
@@ -1,5 +1,5 @@
 <?php
-
+declare(strict_types=1);
 namespace skyss0fly\PortalWarp;
 
 use pocketmine\{plugin\PluginBase, command\CommandSender, command\Command,  player\Player, block\NetherPortal};
@@ -21,6 +21,18 @@ public function onCommand(CommandSender $sender, Command $command, string $label
 
 
   }
+private function registerwarp(){
+$wname = onCommand::warpName();
+       $x = onCommand::$sender->getPosition()->getX();
+        $y = onCommand::$sender->getPosition()->getY();
+        $z = onCommand::$sender->getPosition()->getZ();
+$wcoords = $x . $y . $z
+$warpfolder = file_get_contents('./warps.yml', true);
+  $data = '$wname . $wcoords'
+  file_put_contents($data);
+  
+
+}
 
 
 }
