52f011a50d9f421f0b135de9a252e14a7a71320b
skyss0fly <100278495+skyss0fly@users.noreply.github.com>
2023-08-24 19:51:27 +1000

    Update Main.php

--- src/skyss0fly/PortalWarp/Main.php
+++ src/skyss0fly/PortalWarp/Main.php
@@ -2,7 +2,7 @@
 declare(strict_types=1);
 namespace skyss0fly\PortalWarp;
 
-use pocketmine\{plugin\PluginBase, command\CommandSender, command\Command,  player\Player, block\NetherPortal};
+use pocketmine\{plugin\PluginBase, command\CommandSender, utils\TextFormat command\Command,  player\Player, block\NetherPortal};
 
 
 class Main extends PluginBase {
@@ -18,10 +18,29 @@ $this->saveDefaultConfig();
 public function onCommand(CommandSender $sender, Command $command, string $label, array $args): bool {
         switch ($command->getName()) {
             case "pw":
-
-
+  if (!$sender instanceof Player){
+$sender->sendMessage(TEXTFORMAT::RED . "ERROR: Not In Game");
   }
-private function registerwarp(){
+          if ($args = 0){
+$sender->sendMessage(TEXTFOMAT::YELLOW . "Usage: /pw <command>  \n>> Do /pw help for a list of commands");
+
+          }
+          if ($args = 1) {
+$sender->sendMessage(TEXTFORMAT::YELLOW . "Commands: \n>> /pw setwarp <warpname> \n>> /pw delwarp <warpname> \n>> /pw creatportal <warpname>");
+            
+          }
+             if ($args = 2) {
+               if (!$sender->hasPermission("PortalWarp.setwarp")){
+$sender->sendMessage(TEXTFORMAT::RED . "ERROR: You Dont have permission to use this command!");
+             }
+               else {
+$this->registerWarp($args);
+               }
+          }
+}
+}
+
+private function registerWarp(){
 $wname = onCommand::warpName();
        $x = onCommand::$sender->getPosition()->getX();
         $y = onCommand::$sender->getPosition()->getY();
